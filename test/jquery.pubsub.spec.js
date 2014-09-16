0304f657a9437df1a0b12bae9c7798a8a4338ddf
Phil Gold <pgold@rubixmedia.com>
2014-09-16 15:15:20 -0400

    Adding tests for requestAnimationFrame shim

--- test/jquery.pubsub.spec.js
+++ test/jquery.pubsub.spec.js
@@ -2,6 +2,27 @@
 (function () {
     "use strict";
 
+
+    /**
+     * Test requestAnimationFrame shim
+     */
+    describe("Shim for requestAnimationFrame", function () {
+        it("should create a function on the window object", function () {
+            expect(typeof window.requestAnimFrame).toBe('function');
+        });
+
+        it("should fire a callback", function (done) {
+            window.requestAnimFrame(function () {
+                done();
+            });
+        });
+    });
+
+
+
+    /**
+     * Test jQuery PubSub
+     */
     describe("jQuery PubSub", function () {
         it("should have tests", function () {
             expect(true).toBe(false);
